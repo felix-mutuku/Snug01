ecbfe80408b385e6a5244bcdbb757fa9fb3f5406
Felix Mutuku <38686045+felix-mutuku@users.noreply.github.com>
2018-07-03 15:04:55 +0300

    Added more truth or drink questions and added DARES choice

--- app/src/main/java/com/snugjar/truthordare/PlayCardsActivity.java
+++ app/src/main/java/com/snugjar/truthordare/PlayCardsActivity.java
@@ -32,6 +32,31 @@ public class PlayCardsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences sharedPreferences = getSharedPreferences("PositiveSwipe", Context.MODE_PRIVATE);
+        info = sharedPreferences.getString("choice", "N/A");
+
+        //set theme appropriately of cards to play
+        switch (info) {
+            case "Couples":
+                setTheme(R.style.Couples);
+                break;
+            case "Siblings":
+                setTheme(R.style.Siblings);
+                break;
+            case "Friends":
+                setTheme(R.style.Friends);
+                break;
+            case "Parents":
+                setTheme(R.style.Parents);
+                break;
+            case "Exes":
+                setTheme(R.style.Exes);
+                break;
+            case "Dares":
+                setTheme(R.style.Dares);
+                break;
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_cards);
 
@@ -47,8 +72,8 @@ public class PlayCardsActivity extends AppCompatActivity {
         logo.setTypeface(face);
         resetText.setTypeface(face2);
 
-        SharedPreferences sharedPreferences = getSharedPreferences("PositiveSwipe", Context.MODE_PRIVATE);
-        info = sharedPreferences.getString("choice", "N/A");
+        //SharedPreferences sharedPreferences = getSharedPreferences("PositiveSwipe", Context.MODE_PRIVATE);
+        //info = sharedPreferences.getString("choice", "N/A");
 
         cManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         assert cManager != null;
@@ -81,36 +106,42 @@ public class PlayCardsActivity extends AppCompatActivity {
 
         switch (info) {
             case "Couples":
+                rootLayout.setBackgroundColor(getResources().getColor(R.color.rose_red));
                 for (Choices profile : Objects.requireNonNull(MainUtils.loadCouples(this.getApplicationContext()))) {
                     mSwipeView.addView(new MainPlayCard(mContext, profile, mSwipeView));
                 }
 
                 break;
             case "Siblings":
+                rootLayout.setBackgroundColor(getResources().getColor(R.color.sky_blue));
                 for (Choices profile : Objects.requireNonNull(MainUtils.loadSiblings(this.getApplicationContext()))) {
                     mSwipeView.addView(new MainPlayCard(mContext, profile, mSwipeView));
                 }
 
                 break;
             case "Friends":
+                rootLayout.setBackgroundColor(getResources().getColor(R.color.denim));
                 for (Choices profile : Objects.requireNonNull(MainUtils.loadFriends(this.getApplicationContext()))) {
                     mSwipeView.addView(new MainPlayCard(mContext, profile, mSwipeView));
                 }
 
                 break;
             case "Parents":
+                rootLayout.setBackgroundColor(getResources().getColor(R.color.light_purple));
                 for (Choices profile : Objects.requireNonNull(MainUtils.loadParents(this.getApplicationContext()))) {
                     mSwipeView.addView(new MainPlayCard(mContext, profile, mSwipeView));
                 }
 
                 break;
             case "Exes":
+                rootLayout.setBackgroundColor(getResources().getColor(R.color.black));
                 for (Choices profile : Objects.requireNonNull(MainUtils.loadExes(this.getApplicationContext()))) {
                     mSwipeView.addView(new MainPlayCard(mContext, profile, mSwipeView));
                 }
 
                 break;
             case "Dares":
+                rootLayout.setBackgroundColor(getResources().getColor(R.color.anchor));
                 for (Choices profile : Objects.requireNonNull(MainUtils.loadDares(this.getApplicationContext()))) {
                     mSwipeView.addView(new MainPlayCard(mContext, profile, mSwipeView));
                 }
